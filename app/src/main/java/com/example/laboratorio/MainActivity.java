311aa1bae88b34a04e3ae4689cd2b244d6af8e50
ChristianJoz <65049993+ChristianJoz@users.noreply.github.com>
2020-10-04 23:06:52 -0600

    Ajustes de opciones de menu

--- app/src/main/java/com/example/laboratorio/MainActivity.java
+++ app/src/main/java/com/example/laboratorio/MainActivity.java
@@ -47,9 +47,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.Acercade) {
+            return true;
+        }if (id == R.id.Recicle) {
+            return true;
+        } if (id == R.id.registro) {
             return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
